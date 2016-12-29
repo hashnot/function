3e4282545676cfb63592c6b77e7b8da527f6cbae
Rafał Krupiński <rkrupinski+github@gmail.com>
2016-12-29 15:28:55 +0000

    Extract amqp code to separate file

---
+++ amqp.go
@@ -0,0 +1,103 @@
+package function
+
+import (
+	"encoding/json"
+	"github.com/hashnot/function/amqptypes"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func Start(f Function) error {
+	config := new(amqptypes.Configuration)
+	err := UnmarshalFile("function.yaml", config)
+	if err != nil {
+		return err
+	}
+
+	return StartWithConfig(f, config)
+}
+
+func StartWithConfig(f Function, config *amqptypes.Configuration) error {
+	config.SetupOutputs()
+
+	conn, err := config.Dial()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	outChan, err := SetupOutput(config, ch)
+	if err != nil {
+		return err
+	}
+
+	return ReadInput(config, ch, f, outChan)
+}
+
+func SetupOutput(c *amqptypes.Configuration, ch *amqp.Channel) (OutChan, error) {
+	outChan := make(chan *OutputMessage, 8)
+
+	go func() {
+		for d := range outChan {
+			buf, err := json.Marshal(d.Payload)
+			if err != nil {
+				c.Errors.Publish(ch, []byte(err.Error()))
+			}
+
+			err = c.Output.Publish(ch, buf)
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	}()
+
+	return outChan, nil
+}
+
+func ReadInput(config *amqptypes.Configuration, ch *amqp.Channel, f Function, outChan OutChan) error {
+	msgs, err := config.Input.Consume(ch)
+	if err != nil {
+		return err
+	}
+
+	for d := range msgs {
+		// TODO recover from panic
+		log.Print("input body: ", string(d.Body))
+		//spew.Fdump(os.Stderr, client)
+
+		msg := &Message{d}
+		err = f.Handle(msg, outChan)
+		if err != nil {
+			// TODO handle retryable errors
+			err2 := d.Nack(false, false)
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			config.Errors.Publish(ch, []byte(err.Error()))
+		} else {
+			err2 := d.Ack(false)
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+		}
+	}
+	return nil
+}
+
+type Message struct {
+	delivery amqp.Delivery
+}
+
+func (m *Message) Get() *amqp.Delivery {
+	return &m.delivery
+}
+
+func (m *Message) GetPayload(t interface{}) error {
+	return json.Unmarshal(m.delivery.Body, t)
+}
